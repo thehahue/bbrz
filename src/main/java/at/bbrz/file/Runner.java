c48879406bd5a8681060215f8c2e8932b0e70d16
Stefan Hasenhuetl <hahue@hahue.at>
2022-03-02 10:56:48 +0100

    Change Runner to be runable as script

--- src/main/java/at/bbrz/file/Runner.java
+++ src/main/java/at/bbrz/file/Runner.java
@@ -1,9 +1,20 @@
 package at.bbrz.file;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Runner {
 
     public static void main(String[] args) {
-	    String txt=FileToString.readToString("src/main/resources/Person");
-        System.out.println(txt);
+	   // String txt=FileToString.readToString("src/main/resources/Person");
+        System.out.println("Das hat funktioniert!");
+        try (Stream<String> lines = Files.lines(Paths.get(args[0]))) {
+            System.out.println(lines.collect(Collectors.joining("\n")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
