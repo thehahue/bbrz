da43be5f6a09da0d92464be6144d19ddb99135f8
hazafya <ahazafy@gmail.com>
2022-03-23 14:43:17 +0100

    Add base project files

---
+++ ahazafy/SchoolProject/src/main/java/at/bbrz/SchoolProject/SchoolProjectApplication.java
@@ -0,0 +1,13 @@
+package at.bbrz.SchoolProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolProjectApplication.class, args);
+	}
+
+}
