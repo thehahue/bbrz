76dcece2dd13de037c924daa6d3597222c363da5
Stefan Hasenhuetl <s.hasenhuetl@mediashop-group.com>
2022-03-30 14:27:31 +0200

    Add base services.

---
+++ stefanH/SchoolProject/src/main/java/at/bbrz/schoolbackend/service/LehrerService.java
@@ -0,0 +1,17 @@
+package at.bbrz.schoolbackend.service;
+
+import at.bbrz.schoolbackend.model.Lehrer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LehrerService {
+    @Autowired
+    private SchuleService schuleService;
+
+    public List<Lehrer> getLehrer() {
+        return schuleService.getSchule().getLehrer();
+    }
+}
