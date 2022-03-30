70234b8826448e7495a914abb6d013bb121ab35f
Ferum1995 <80038402+Ferum1995@users.noreply.github.com>
2022-03-30 14:51:41 +0200

    Person Interface added

---
+++ kevinK/RestInterface/RestInterface/Interfaces/IPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestInterface.Interfaces
+{
+    public interface IPerson
+    {
+        int id { get; set; }
+        string firstName { get; set; }
+        string lastName { get; set; }
+        DateTime birthDate { get; set; }
+
+    }
+}
