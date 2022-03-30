636ff64ec4c51667e0e57a6ed1b996f6c5c0202a
Ferum1995 <80038402+Ferum1995@users.noreply.github.com>
2022-03-30 14:51:54 +0200

    Models Added

---
+++ kevinK/RestInterface/RestInterface/Models/StudentModel.cs
@@ -0,0 +1,27 @@
+﻿using RestInterface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestInterface.Models
+{
+    public class StudentModel : IPerson
+    {
+        public int id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string firstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string lastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime birthDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int studentId;
+
+        public StudentModel(int id, string firstName, string lastName, DateTime birthDate, int studentId)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.studentId = studentId;
+        }
+    }
+}
