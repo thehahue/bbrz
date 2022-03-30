0b49aba775d8c64d2955719b7763ad19bd62b844
Stefan Hasenhuetl <s.hasenhuetl@mediashop-group.com>
2022-03-30 14:27:12 +0200

    Add Schule, Klasse, Raum classes.

---
+++ stefanH/SchoolProject/src/main/java/at/bbrz/schoolbackend/model/Raum.java
@@ -0,0 +1,14 @@
+package at.bbrz.schoolbackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Raum {
+    private String id;
+    private String name;
+
+}
