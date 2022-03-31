66acf6ad64aabd03526943b00ed2168124148dc9
IvicaKATIC <79213308+IvicaKATIC@users.noreply.github.com>
2022-03-31 09:18:12 +0200

    class interface Person created

---
+++ ikatic/SchoolProject/src/main/java/at/bbrz/schoolbackend/model/Person.java
@@ -0,0 +1,10 @@
+package at.bbrz.schoolbackend.model;
+
+import java.time.LocalDate;
+
+public interface Person {
+    String getId();
+    String getFirstName();
+    String getLastName();
+    LocalDate dateOfBirth();
+}
