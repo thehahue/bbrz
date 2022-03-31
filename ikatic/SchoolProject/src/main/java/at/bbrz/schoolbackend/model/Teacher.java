cb6fb8129c965c4b832343294d60ee430a5a0274
IvicaKATIC <79213308+IvicaKATIC@users.noreply.github.com>
2022-03-31 10:02:20 +0200

    class Student created

---
+++ ikatic/SchoolProject/src/main/java/at/bbrz/schoolbackend/model/Teacher.java
@@ -0,0 +1,18 @@
+package at.bbrz.schoolbackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Teacher implements Person{
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String mainSubject;
+}
