5beb8806887ec1cd5ea748f647858dc793d3e85d
IvicaKATIC <79213308+IvicaKATIC@users.noreply.github.com>
2022-03-31 09:42:02 +0200

    class Room created

---
+++ ikatic/SchoolProject/src/main/java/at/bbrz/schoolbackend/model/Room.java
@@ -0,0 +1,13 @@
+package at.bbrz.schoolbackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Room {
+    private String id;
+    private String name;
+}
