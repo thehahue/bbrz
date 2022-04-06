b806956ff7b6b2de40180ccb383aadd5c93501a0
seppi <sebastian.bloder97@gmail.com>
2022-04-06 15:05:01 +0200

    Created Client Class (Main)

---
+++ SchoolProject/src/main/java/at/bbrz/schoolproject/Client.java
@@ -0,0 +1,10 @@
+package at.bbrz.schoolproject;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        Student hansi = new Student(1,"hansi","graf", "19.02.2001");
+
+    }
+}
