05e0c68d6460828bf4e949599ac7a7806fdf7c41
Ferum1995 <80038402+Ferum1995@users.noreply.github.com>
2022-04-06 15:29:31 +0200

    TeacherController (Get) angefangen zu erstellen

--- kevinK/SchoolApi/SchoolApi/Service/TeacherService.cs
+++ kevinK/SchoolApi/SchoolApi/Service/TeacherService.cs
@@ -9,23 +9,6 @@ namespace SchoolApi.Service
 {
     public class TeacherService
     {
-        static HttpClient client = new HttpClient();
 
-
-        static async Task<TeacherModel> getTeacher(string path)
-        {
-            TeacherModel teacher = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            string teacherString = "";
-
-            if (response.IsSuccessStatusCode)
-            {
-                teacherString = await response.Content.ReadAsStringAsync();
-            }
-
-            Console.WriteLine(teacherString);
-
-            return teacher;
-        }
     }
 }
