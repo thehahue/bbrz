e3fc860547bf67ae772b26f3ed90255b0e76fc14
hazafya <ahazafy@gmail.com>
2022-04-06 14:58:08 +0200

    created class "school".
    added parameters

---
+++ ahazafy/SchoolProject/src/main/java/at/bbrz/SchoolProject/School.java
@@ -0,0 +1,16 @@
+package at.bbrz.SchoolProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+
+    private List<Room> rooms = new ArrayList<>();
+    private List<Teacher> teachers = new ArrayList<>();
+    private List<Form> forms =new ArrayList<>();
+
+    private String id;
+    private Teacher headMaster;
+
+
+}
