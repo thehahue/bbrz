e5a0e2fa8574927cb1c85689e32e408353c8cf23
Ferum1995 <80038402+Ferum1995@users.noreply.github.com>
2022-04-06 15:00:27 +0200

    Recreate Api again as asp.net and Delete old File proberly

---
+++ kevinK/SchoolApi/SchoolApi/Service/TeacherService.cs
@@ -0,0 +1,31 @@
+﻿using SchoolApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SchoolApi.Service
+{
+    public class TeacherService
+    {
+        static HttpClient client = new HttpClient();
+
+
+        static async Task<TeacherModel> getTeacher(string path)
+        {
+            TeacherModel teacher = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            string teacherString = "";
+
+            if (response.IsSuccessStatusCode)
+            {
+                teacherString = await response.Content.ReadAsStringAsync();
+            }
+
+            Console.WriteLine(teacherString);
+
+            return teacher;
+        }
+    }
+}
