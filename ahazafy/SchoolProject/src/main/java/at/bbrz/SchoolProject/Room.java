8fbaa55a9ecabeb1babe4bb9db4686622eaa4139
hazafya <ahazafy@gmail.com>
2022-04-06 14:47:36 +0200

    added constructor

---
+++ ahazafy/SchoolProject/src/main/java/at/bbrz/SchoolProject/Room.java
@@ -0,0 +1,11 @@
+package at.bbrz.SchoolProject;
+
+public class Room {
+    private String id;
+    private String name;
+
+    public Room(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
