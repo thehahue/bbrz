2e60b015bfc3fcb981a65a8d4810b8d61b800a09
akrall86 <itn234551@qualifizierung.at>
2022-04-06 13:52:32 +0200

    Add Cunstructor and Getter/Setter

--- andi/SchoolProject/src/main/java/at/bbrz/schoolbackend/model/Pupils.java
+++ andi/SchoolProject/src/main/java/at/bbrz/schoolbackend/model/Pupils.java
@@ -1,14 +1,51 @@
 package at.bbrz.schoolbackend.model;
 
-import lombok.Data;
 
 import java.util.Date;
 
-@Data
+
 public class Pupils implements Person{
     private int id;
-    private String firstNama;
+    private String firstName;
     private String lastName;
     private Date birthday;
 
+    public Pupils(int id, String firstName, String lastName, Date birthday) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstNama(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 }
