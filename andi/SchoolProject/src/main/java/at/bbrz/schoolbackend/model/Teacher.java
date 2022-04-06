8a977b88fe42b3d9fd3c899fecc9a816831557f7
akrall86 <itn234551@qualifizierung.at>
2022-04-06 13:54:07 +0200

    Add Constructor and Getter/Setter

--- andi/SchoolProject/src/main/java/at/bbrz/schoolbackend/model/Teacher.java
+++ andi/SchoolProject/src/main/java/at/bbrz/schoolbackend/model/Teacher.java
@@ -1,13 +1,50 @@
 package at.bbrz.schoolbackend.model;
 
-import lombok.Data;
 
 import java.util.Date;
 
-@Data
 public class Teacher implements Person {
     private int id;
-    private String firstname;
+    private String firstName;
     private String lastname;
     private Date birthday;
+
+    public Teacher(int id, String firstName, String lastname, Date birthday) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.birthday = birthday;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 }
