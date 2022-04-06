bd999b3e7a4cbeae7ebde1ed8f25f0e568ea0f9f
Ferum1995 <80038402+Ferum1995@users.noreply.github.com>
2022-04-06 14:40:37 +0200

    Revert "Recreate Api as  asp.net core Delete old Files"
    This reverts commit fc0c4410cce78c8c7fb427738a74c0e6a668e71a.

---
+++ kevinK/RestInterface/RestInterface/Models/SchoolModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestInterface.Models
+{
+    public class SchoolModel
+    {
+        public int id;
+        public List<RoomModel> rooms;
+        public List<ClassModel> classes;
+        public List<TeacherModel> teachers;
+        public TeacherModel director;
+
+        public SchoolModel(int id, List<RoomModel> rooms, List<ClassModel> classes, List<TeacherModel> teachers, TeacherModel director)
+        {
+            this.id = id;
+            this.rooms = rooms;
+            this.classes = classes;
+            this.teachers = teachers;
+            this.director = director;
+
+        }
+
+    }
+}
